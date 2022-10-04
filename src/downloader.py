26f81b7682c346604dce2bfc47c6abadd04820a4
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-10-04 11:17:34 +0300

    added experimental folder traversing

--- src/downloader.py
+++ src/downloader.py
@@ -8,16 +8,25 @@ SINIF_DOSYALARI_URL_EXTENSION = "/SinifDosyalari"
 def download_all(session, course):
     global URL
     download_directory = fd.askdirectory()
-    files = BeautifulSoup(session.get(URL + course.link + SINIF_DOSYALARI_URL_EXTENSION).content.decode("utf-8"), "lxml")
+    raw_html = session.get(URL + course.link + SINIF_DOSYALARI_URL_EXTENSION).content.decode("utf-8")
+    _download_or_traverse(session, raw_html, download_directory)
+    
+
+def _download_or_traverse(session, raw_html, destionation_folder):
+    files = BeautifulSoup(raw_html, "lxml")
     files = files.select_one(".dosyaSistemi table.data").find_all("tr")
-    files.pop(0)
+    files.pop(0) # first row is the header of the table
+
     for file in files:
         file_link = file.find("td").find("a")["href"]
         resp = session.get(URL + file_link)
-        file_name_offset = resp.headers["content-disposition"].index("filename=") + 9
-        file_name = resp.headers["content-disposition"][file_name_offset:]
-        with open(download_directory + "/" + file_name, "wb") as bin:
-            bin.write(resp.content)
+        if "text/html" in resp.headers["content-type"]:
+            _download_or_traverse(session, resp.content.decode("utf-8"), destionation_folder)
+        else:
+            file_name_offset = resp.headers["content-disposition"].index("filename=") + 9
+            file_name = resp.headers["content-disposition"][file_name_offset:]
+            with open(destionation_folder + "/" + file_name, "wb") as bin:
+                bin.write(resp.content)
 
 
 
