ce2869c8ac37a7b931e0aa50e4bfc3154922389d
Ahmet Hakan Aktuğ <ahmethakanaktug@gmail.com>
2022-11-05 16:14:57 +0300

    basic file adding and checking to db

--- src/db_handler.py
+++ src/db_handler.py
@@ -1,6 +1,8 @@
 import sqlite3
 from multiprocessing import Queue
-from os.path import join
+from os.path import join, exists
+from enum import Enum
+from zlib import crc32
 
 from src.configuration import Config
 from src import logger
@@ -10,11 +12,17 @@ TABLE_CREATION_QUERY = "CREATE TABLE files (id INTEGER PRIMARY KEY, path TEXT UN
 TABLE_CHECK_QUERY = (
     "SELECT name FROM sqlite_master WHERE type='table' AND name='files';"
 )
+SELECT_FILE_BY_ID_QUERY = "SELECT isDeleted, id FROM files WHERE id = ?"
+FILE_INSERTION_QUERY = "INSERT INTO files (id, path, hash) VALUES (?, ?, ?)"
+
+class FILE_STATUS(Enum):
+    NEW = 0
+    DELETED = 1
+    EXISTS = 2
 
 
 class DB:
     connection: sqlite3.Connection
-    cursor: sqlite3.Cursor
     to_add: Queue = Queue()
 
     @classmethod
@@ -22,18 +30,57 @@ class DB:
         db_path = join(base_directory, DATABASE_FILE_NAME)
         try:
             cls.connection = sqlite3.connect(db_path)
-            cls.cursor = cls.connection.cursor()
+            cursor = cls.connection.cursor()
             logger.debug("Veri tabanına bağlanıldı.")
         except:
             logger.fail("Veri tabanına bağlanılamadı.")
 
         if Config.first_run:
-            cls.cursor.execute(TABLE_CREATION_QUERY)
+            cursor.execute(TABLE_CREATION_QUERY)
             logger.info("Veri tabanı ilk çalıştırma için hazırlandı.")
         else:
-            cls.cursor.execute(TABLE_CHECK_QUERY)
-            if cls.cursor.getCount() < 1:
+            cursor.execute(TABLE_CHECK_QUERY)
+            if cursor.getCount() < 1:
                 logger.fail(
                     "Veri tabanı bozulmuş. 'ninova_arsivci.db' dosyasını silip tekrar başlatın. Silme işlemi sonrasında tüm dosyalar yeniden indirilir."
                 )
+
+        cursor.close()
+    
+    # takes file_id, finds and returns the status from database
+    # file_id is the end of the file url (after question mark - question mark and 'g' is not included)
+    @classmethod
+    def check_file_status(cls, file_id:int, cursor: sqlite3.Cursor):
+        cursor.execute(SELECT_FILE_BY_ID_QUERY, (file_id,))
+        if cursor.getCount() > 0:
+            for deleted, id in cursor:
+                if file_id != id:
+                    logger.fail("Eş zamanlı erişim, race condition oluşturdu. Veri tabanından gelen bilgi, bu dosyaya ait değil.")
+                if deleted:
+                    return FILE_STATUS.DELETED
+                else:
+                    return FILE_STATUS.EXISTS
+        else:
+            return FILE_STATUS.NEW
+
+
+    # Should be called after the download
+    @classmethod
+    def add_file(cls, id: int, path: str, cursor: sqlite3.Cursor):
+        if exists(path):
+            with open(path, "rb") as file:
+                hash = crc32(file.read())
+                cursor.execute(FILE_INSERTION_QUERY, (id, path, hash))
+        else:
+            logger.fail("Given file to add to DB, cannot found in disk.")
+
+    @classmethod
+    def apply_changes_and_close(cls):
+        cls.connection.commit()
+        cls.connection.close()
+
+    @classmethod
+    def get_new_cursor(cls):
+        return cls.connection.cursor()
+
 
